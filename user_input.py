a7e2148d1e611b024f4249b22543da89a3b50124
Babajaden <proffesordique@gmail.com>
2024-03-06 06:39:30 -0800

    my first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = str(input("State your name: "))
+age = int(input("State your age: "))
+location = str(input("State your location: "))
+
+print(f"Hello {name}, your age {age} years old and live in {location}")
+
