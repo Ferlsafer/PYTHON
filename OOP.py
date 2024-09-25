455928cd19cb839c25db882ccadb33db3741df68
Ferlsafer <proffesordique@gmail.com>
2024-09-26 02:04:40 +0300

    adding some comments

--- OOP.py
+++ OOP.py
@@ -1,5 +1,6 @@
 '''
 working with classes (_init_ function)
+The _init_ is used to initialize the fields of the class, it is used as a constructor
 '''
 
 class Staff:
@@ -29,3 +30,10 @@ class Students:
     
 student1 = Students('John Axon', 'Computer Science', 16)
 print(student1)
+
+'''
+The key difference between the above two classes is the use of _str_ function and without _str_ function
+-when using str function you give Python a power to print result(string) in human readable format
+-without using str the Python will return the memory addres which may confuse us
+-the way we acces is different, I have printed each value different, when I did not use str and print all value of student when using str
+'''
