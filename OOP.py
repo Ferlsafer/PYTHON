a57b0b07c9b5efb4a38655c669921bf3e98b3b45
Ferlsafer <proffesordique@gmail.com>
2024-09-26 03:13:37 +0300

    adding class methods

--- OOP.py
+++ OOP.py
@@ -37,3 +37,20 @@ The key difference between the above two classes is the use of _str_ function an
 -without using str the Python will return the memory addres which may confuse us
 -the way we acces is different, I have printed each value different, when I did not use str and print all value of student when using str
 '''
+
+'''
+Methods - are functions which belongs to the specific object
+'''
+class Addition():
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        
+    def DisplayResult(self):
+        result = self.num1 + self.num2
+        print(f"The result is {result}")
+
+addNumber = Addition(40, 30)
+addNumber.DisplayResult()
+
+
